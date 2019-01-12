e7dbc05e3a09a395ab1be4290976fadeacfab7d2
2637309949@qq.com <2637309949@qq.com>
2019-01-13 01:37:23 +0800

    上传插件

---
+++ plugins/upload.go
@@ -0,0 +1,65 @@
+/**
+ * @author [Double]
+ * @email [2637309949@qq.com.com]
+ * @create date 2019-01-12 22:46:31
+ * @modify date 2019-01-12 22:46:31
+ * @desc [bulrush Upload plugin]
+ */
+
+package plugins
+
+import (
+	"github.com/2637309949/bulrush/utils"
+	"path"
+	"path/filepath"
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+ 
+// Upload -
+type Upload struct {
+	AssetPath string
+	URLPrefix string
+}
+
+ // Plugin -
+func (upload *Upload) Plugin (router *gin.RouterGroup) {
+	router.POST("/upload", func(c *gin.Context) {
+		// name := c.PostForm("name")
+		// email := c.PostForm("email")
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+			return
+		}
+		rets := make([] map[string] interface{}, 0)
+		for _, files := range form.File {
+			for _, file := range files {
+				filename := filepath.Base(file.Filename)
+				uuid := utils.RandString(32)
+				uuidFileName := utils.RandString(32) + string(filename[len(filename)-len(filepath.Ext(filename)):])
+				if err := c.SaveUploadedFile(file, path.Join(upload.AssetPath, uuidFileName)); err != nil {
+					c.JSON(http.StatusOK, gin.H{
+						"data": 	nil,
+						"errcode": 	500,
+						"errmsg": 	err.Error(),
+					})
+					return
+				}
+				ret := map[string] interface{} {
+					"uid": 	  uuid,
+					"status": "done",
+					"name":   filename,
+					"url":    upload.URLPrefix + "/" + uuidFileName,
+				}
+				rets = append(rets, ret)
+			}
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": 	nil,
+			"errcode": 	nil,
+			"errmsg": 	rets,
+		})
+	})
+}

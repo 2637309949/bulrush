1a40a2998d8a7c3c0c3a3d9d14cff02c78111a7a
2637309949@qq.com <2637309949@qq.com>
2019-01-16 10:54:20 +0800

    优化框架

--- plugins/override.go
+++ plugins/override.go
@@ -14,41 +14,39 @@ import (
 )
 
 // Override for gin
-func Override() func (*gin.RouterGroup, *gin.Engine) {
-	return func (router *gin.RouterGroup, httpProxy *gin.Engine) {
-		httpProxy.Use(func(c *gin.Context) {
-			if c.Request.Method != "POST" {
-				c.Next()
-			} else {
-				method := c.PostForm("_method")
-				methods := [3]string{"DELETE", "PUT", "PATCH"}
-				if method != "" {
-					for _, target := range methods {
-						if(target == strings.ToUpper(method)) {
-							c.Request.Method = target
-							httpProxy.HandleContext(c)
-							break
-						}
+func Override(router *gin.RouterGroup, httpProxy *gin.Engine) {
+	httpProxy.Use(func(c *gin.Context) {
+		if c.Request.Method != "POST" {
+			c.Next()
+		} else {
+			method := c.PostForm("_method")
+			methods := [3]string{"DELETE", "PUT", "PATCH"}
+			if method != "" {
+				for _, target := range methods {
+					if(target == strings.ToUpper(method)) {
+						c.Request.Method = target
+						httpProxy.HandleContext(c)
+						break
 					}
 				}
 			}
-		})
-		router.Use(func(c *gin.Context) {
-			if c.Request.Method != "POST" {
-				c.Next()
-			} else {
-				method := c.PostForm("_method")
-				methods := [3]string{"DELETE", "PUT", "PATCH"}
-				if method != "" {
-					for _, target := range methods {
-						if(target == strings.ToUpper(method)) {
-							c.Request.Method = target
-							httpProxy.HandleContext(c)
-							break
-						}
+		}
+	})
+	router.Use(func(c *gin.Context) {
+		if c.Request.Method != "POST" {
+			c.Next()
+		} else {
+			method := c.PostForm("_method")
+			methods := [3]string{"DELETE", "PUT", "PATCH"}
+			if method != "" {
+				for _, target := range methods {
+					if(target == strings.ToUpper(method)) {
+						c.Request.Method = target
+						httpProxy.HandleContext(c)
+						break
 					}
 				}
 			}
-		})
-	}
+		}
+	})
 }

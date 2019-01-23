9c1993604ba1cc1b9fa5b3a2697446253da205be
2637309949@qq.com <2637309949@qq.com>
2019-01-23 16:36:51 +0800

    add benchmarks

--- benchmarks/middleware.go
+++ benchmarks/middleware.go
@@ -20,16 +20,21 @@ func main() {
   app.Inject("bulrushApp")
 
   for ;n > 0; n = n -1 {
-    app.Use(bulrush.PNQuick(func(testInject string, router *gin.RouterGroup) {
-      router.GET("/", func (c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-          "data": 	testInject,
-          "errcode": 	nil,
-          "errmsg": 	nil,
-        })
+    app.Use(bulrush.PNQuick(func (httpProxy *gin.Engine, router *gin.RouterGroup) {
+      httpProxy.Use(func (c *gin.Context) {
+        c.Next()
       })
-    }))
+		}))
   }
+  app.Use(bulrush.PNQuick(func(testInject string, router *gin.RouterGroup) {
+    router.GET("/", func (c *gin.Context) {
+      c.JSON(http.StatusOK, gin.H{
+        "data": 	testInject,
+        "errcode": 	nil,
+        "errmsg": 	nil,
+      })
+    })
+  }))
   app.Run(func(err error, config *bulrush.Config) {
     if err != nil {
       panic(err)
